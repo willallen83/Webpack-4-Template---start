413cf628df6531b36a776a80a6eed9b3b3d5cce4
Will Allen <will@enlightened-designs.com>
2018-07-02 18:59:48 +0200

    Creating Repository

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import style from './sass/style.scss';
+
+const myModule = require('./myModule');
+
+const bar = myModule.foo();
+
+console.log(bar);
---
+++ src/myModule.js
@@ -0,0 +1,5 @@
+module.exports = {
+  foo: () => {
+    return 'hello (console) world';
+  },
+};
